6621b00e54cede413c42f53c7a3a48a76f7e21ef
«Alicia» <alicia@ringover.com>
2022-05-14 00:16:33 +0200

    add fact, remindFact, deleteAsked functions

---
+++ go.mod
@@ -0,0 +1,34 @@
+module worker
+
+go 1.17
+
+require (
+	github.com/TavernierAlicia/eap-FACT v0.0.0-20220513123934-7797e4bb8dec // indirect
+	github.com/TavernierAlicia/eap-MAIL v0.0.0-20220513215433-303ce754cc1c // indirect
+	github.com/boombuler/barcode v1.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/johnfercher/maroto v0.37.0 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/ruudk/golang-pdf417 v0.0.0-20201230142125-a7e3863a1245 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.11.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20220512140231-539c8e751b99 // indirect
+)
---
+++ worker.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	eapFact "github.com/TavernierAlicia/eap-FACT"
+	eapMail "github.com/TavernierAlicia/eap-MAIL"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// connect database
+	//// IMPORT CONFIG ////
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		fmt.Println("reading config file failed ", err)
+	}
+
+	//// DB CONNECTION ////
+	pathSQL := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", viper.GetString("database.user"), viper.GetString("database.pass"), viper.GetString("database.host"), viper.GetInt("database.port"), viper.GetString("database.dbname"))
+	db, err := sqlx.Connect("mysql", pathSQL)
+
+	if err != nil {
+		fmt.Println("failed to connect to database ", err)
+	}
+
+	// Make fact
+	err = fact(db)
+	if err != nil {
+		fmt.Println("Fact clients failed ", err)
+	}
+
+	err = remindFact(db)
+	if err != nil {
+		fmt.Println("Cannot remind fact to clients: ", err)
+	}
+
+	err = deleteAsked(db)
+	if err != nil {
+		fmt.Println("Delete accounts failed")
+	}
+}
+
+// Fact
+func fact(db *sqlx.DB) (err error) {
+	// select etabs_id who need fact
+	var ids []int
+	err = db.Select(&ids, "SELECT id FROM etabs WHERE DATE(created_at) = DATE(NOW()) AND suspended != 1 AND offer IS NOT NULL")
+
+	if len(ids) >= 1 {
+		for _, id := range ids {
+			var etab eapFact.FactEtab
+
+			// get data to create fact
+			err = db.Get(&etab, "SELECT name, owner_civility, owner_name, owner_surname, mail, phone, fact_addr, fact_cp, fact_city, fact_country, offer FROM etabs WHERE etabs.id = ?", id)
+
+			if err != nil {
+				report(db, id, 0, "GET_FAILED")
+				fmt.Println("Get etab data failed ", err)
+			} else {
+				// get offer
+				err = db.Get(&etab.Etab_offer, "SELECT id, name, priceHT, priceTTC FROM offers WHERE id = ?", etab.Offer)
+				if err != nil {
+					report(db, id, 0, "GET_FAILED")
+				} else {
+					etab.Fact_infos.Uuid = uuid.New().String()
+					etab.Fact_infos.IsFirst = true
+					etab.Fact_infos.Date = time.Now().Format("02-01-2006")
+					etab.Fact_infos.Link = "./media/factures/" + etab.Fact_infos.Uuid + "_" + etab.Fact_infos.Date + ".pdf"
+
+					// create new fact
+					insertFact, err := db.Exec("INSERT INTO factures (uuid, etab_id, link, created) VALUES (?, ?, ?, NOW())", etab.Fact_infos.Uuid, id, etab.Fact_infos.Link)
+					if err != nil {
+						report(db, id, etab.Fact_infos.Id, "INSERT_FAILED")
+					} else {
+						factID, err := insertFact.LastInsertId()
+						if err != nil {
+							report(db, id, etab.Fact_infos.Id, "INSERT_FAILED")
+						} else {
+							etab.Fact_infos.Id = factID
+							err = eapFact.CreateFact(etab)
+							if err != nil {
+								report(db, id, etab.Fact_infos.Id, "CREATE_FAILED")
+							} else {
+								err = eapMail.SendBossFact(etab)
+								if err != nil {
+									report(db, id, etab.Fact_infos.Id, "SEND_FAILED")
+								} else {
+									report(db, id, etab.Fact_infos.Id, "SENDED")
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return err
+}
+
+// Fact reminder
+func remindFact(db *sqlx.DB) (err error) {
+	// select etabs who didn't pay
+	var ids []int
+	err = db.Select(&ids, "SELECT etab_id FROM factures JOIN etabs ON etabs.id = factures.etab_id WHERE payed is NULL AND etabs.suspended != 1 GROUP BY etab_id")
+	if len(ids) >= 1 {
+		for _, id := range ids {
+			// get more details
+			var etab eapFact.FactEtab
+			err = db.Get(&etab, "SELECT name, owner_civility, owner_name, owner_surname, mail, phone, fact_addr, fact_cp, fact_city, fact_country, offer FROM etabs WHERE etabs.id = ?", id)
+			if err != nil {
+				fmt.Println("cannot get infos: ", err)
+			} else {
+				var unpaid eapMail.Unpaid
+				err = db.Get(&unpaid, `
+					SELECT 
+						COUNT(factures.id) AS number,
+						(offers.priceTTC * COUNT(factures.id)) AS total
+					FROM factures 
+						JOIN etabs ON etabs.id = factures.etab_id 
+						JOIN offers ON offers.id = etabs.offer
+					WHERE factures.payed is NULL AND etab_id = ?`,
+					id)
+
+				if err != nil {
+					fmt.Println("Cannot get unpayed: ", err)
+				} else {
+					if unpaid.Number > 2 {
+						_, err = db.Exec("UPDATE etabs SET suspended = 1 WHERE id = ?", id)
+						if err != nil {
+							fmt.Println("accunt suspend failed: ", err)
+						} else {
+							err = eapMail.SuspendCreanceMail(etab, unpaid)
+							if err != nil {
+								fmt.Println("send warning failed: ", err)
+							}
+						}
+					} else {
+						var result []string
+						err = db.Select(&result, "SELECT link FROM factures WHERE payed is NULL AND etab_id = ?", id)
+						if err != nil {
+							fmt.Println("cannot get fact links: ", err)
+						} else {
+							unpaid.Facts = result
+							// send mail now
+							err = eapMail.CreanceMail(etab, unpaid)
+							if err != nil {
+								fmt.Println("cannot send fact reminder: ", err)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return err
+}
+
+// Report bug
+func report(db *sqlx.DB, etab_id int, fact_id int64, status string) {
+	_, err := db.Exec("INSERT INTO fact_logs (fact_id, etab_id, status, created_at) VALUES (?, ?, ?, NOW())", fact_id, etab_id, status)
+
+	if err != nil {
+		fmt.Println("Error logging send results: ", err)
+	}
+}
+
+func deleteAsked(db *sqlx.DB) (err error) {
+	var ids []int
+	// get accounts to delete
+	err = db.Select(&ids, "SELECT id FROM etabs WHERE DATE(created_at) = DATE(NOW()) AND offer IS NULL")
+
+	if len(ids) >= 1 {
+		for _, id := range ids {
+
+			// get more infos
+			var etab eapFact.FactEtab
+			err = db.Get(&etab, "SELECT name, owner_civility, owner_name, owner_surname, mail, phone, fact_addr, fact_cp, fact_city, fact_country, 0 AS offer FROM etabs WHERE etabs.id = ?", id)
+			if err != nil {
+				fmt.Println("cannot get infos: ", err)
+			} else {
+				// delete all data for this account
+				_, err = db.Exec("DELETE FROM conections WHERE etab_id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting connections: ", err)
+				}
+				_, err = db.Exec("DELETE FROM factures WHERE etab_id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting factures: ", err)
+				}
+				_, err = db.Exec("DELETE FROM fact_logs WHERE etab_id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting fact_logs: ", err)
+				}
+				_, err = db.Exec("DELETE FROM items WHERE etab_id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting items: ", err)
+				}
+				_, err = db.Exec("DELETE orders, order_items FROM orders INNER JOIN order_items WHERE orders.id = order_items.order_id AND etab_id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting orders and order_items: ", err)
+				}
+				_, err = db.Exec("DELETE FROM planning WHERE etab_id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting planning: ", err)
+				}
+				_, err = db.Exec("DELETE FROM qr_tokens WHERE etab_id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting qr_tokens: ", err)
+				}
+				_, err = db.Exec("DELETE FROM etabs WHERE id = ?", id)
+				if err != nil {
+					fmt.Println("Error deleting etabs: ", err)
+				}
+			}
+			// now send mail
+			err = eapMail.ConfirmDeleteAccount(etab)
+			if err != nil {
+				fmt.Println("send mail delete failed")
+			}
+		}
+	}
+	return err
+}
